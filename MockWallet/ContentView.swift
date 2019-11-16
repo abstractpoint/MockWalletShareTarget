069b0d6276e250e5bf26400357425d80d77210d7
George <george@abstractpoint.co.uk>
2019-11-16 18:45:27 +0000

    Initial Commit

---
+++ MockWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MockWallet
+//
+//  Created by George T on 16/11/2019.
+//  Copyright © 2019 George T. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
